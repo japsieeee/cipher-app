5d808d847699dcfc2945586862058c68410e9584
JP JAPOS <dyollsopaj@gmail.com>
2023-09-26 14:33:07 +0800

    [upload] done building simple app

---
+++ CipherApp/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Hasher = AMS.Hasher.Hashers;
+
+namespace CipherApp
+{
+    public partial class MainPage : ContentPage
+    {
+        string HashedValue;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConvert_Clicked(object sender, EventArgs e)
+        {
+            BtnCopy.Text = "Copy";
+            BtnCopy.IsEnabled = true;
+
+            if (Editor.Text.Length > 0)
+            {
+                var rot13 = new Hasher.ROT13();
+                HashedValue = rot13.Hash(Editor.Text);
+                Label.Text = HashedValue;
+                Frame.IsVisible = true;
+            } else
+            {
+                Frame.IsVisible = false;
+            }
+        }
+
+        private async void BtnCopy_Clicked(object sender, EventArgs e)
+        {
+            await Clipboard.SetTextAsync(HashedValue);
+            BtnCopy.Text = "Copied";
+            BtnCopy.IsEnabled = false;
+        }
+    }
+}
---
+++ CipherApp/MauiProgram.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace CipherApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            Microsoft.Maui.Handlers.EditorHandler.Mapper.AppendToMapping(nameof(Editor), (handler, view) =>
+            {
+#if ANDROID
+            handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+#endif
+            });
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
